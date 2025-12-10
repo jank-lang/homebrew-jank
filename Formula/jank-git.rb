11c1b74e39e02190577453946f98925452d6f35d
Oscar Vargas Torres <contact@oscarvarto.mx>
2025-12-09 22:11:33 -0600

    Update jank formulas with environment fixes for LLVM HEAD
    - Add jank-git formula for building from source with LLVM HEAD
    - Update jank binary formula with proper environment configuration
    - Set proper include/library paths for LLVM's bundled libc++ and libunwind
    - Update README with installation instructions

--- Formula/jank-git.rb
+++ Formula/jank-git.rb
@@ -1,4 +1,4 @@
-class Jank < Formula
+class JankGit < Formula
   desc "The native Clojure dialect hosted on LLVM with seamless C++ interop."
   homepage "https://jank-lang.org"
   url "https://github.com/jank-lang/jank.git", branch: "main"
@@ -11,34 +11,53 @@ class Jank < Formula
 
   depends_on "boost"
   depends_on "libzip"
-  depends_on "llvm@21"
+  depends_on "llvm" => :head
   depends_on "openssl"
 
   skip_clean "bin/jank"
 
   def install
-    ENV.prepend_path "PATH", Formula["llvm@21"].opt_bin
+    llvm = Formula["llvm"]
+    ENV.prepend_path "PATH", llvm.opt_bin
+    ENV["CC"] = llvm.opt_bin/"clang"
+    ENV["CXX"] = llvm.opt_bin/"clang++"
 
-    ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["llvm@21"].opt_lib}"
+    llvm_include = llvm.opt_include
+    llvm_lib = llvm.opt_lib
 
-    ENV.append "CPPFLAGS", "-L#{Formula["llvm@21"].opt_include}"
-    ENV.append "CPPFLAGS", "-fno-sized-deallocation"
+    # Build flags with proper header search order
+    # libc++ headers must come BEFORE SDK C headers
+    ENV["CPPFLAGS"] = "-isystem #{llvm_include}/c++/v1 -I#{llvm_include}"
+    ENV["CXXFLAGS"] = "-isystem #{llvm_include}/c++/v1"
+    ENV["LDFLAGS"] = "-L#{llvm_lib} -L#{llvm_lib}/c++ -L#{llvm_lib}/unwind -Wl,-rpath,#{llvm_lib} -Wl,-rpath,#{llvm_lib}/c++ -Wl,-rpath,#{llvm_lib}/unwind"
 
-    if OS.mac?
-      ENV["SDKROOT"] = MacOS.sdk_path
-    else
-      ENV["CC"] = Formula["llvm@21"].opt_bin/"clang"
-      ENV["CXX"] = Formula["llvm@21"].opt_bin/"clang++"
+    configure_args = [
+      "-GNinja",
+      *std_cmake_args,
+      "-DHOMEBREW_ALLOW_FETCHCONTENT=ON",
+      "-DCMAKE_CXX_COMPILER=#{llvm.opt_bin}/clang++",
+      "-DCMAKE_C_COMPILER=#{llvm.opt_bin}/clang",
+      "-DLLVM_DIR=#{llvm_lib}/cmake/llvm",
+      "-DClang_DIR=#{llvm_lib}/cmake/clang",
+      "-DMLIR_DIR=#{llvm_lib}/cmake/mlir",
+    ]
+
+    # Use Dir.chdir block form to avoid Ruby warning about nested chdir
+    Dir.chdir("compiler+runtime") do
+      system "./bin/configure", *configure_args
+      system "./bin/compile"
+      system "./bin/install"
     end
 
-    cd "compiler+runtime"
-
-    system "./bin/configure",
-           "-GNinja",
-           *std_cmake_args,
-           "-DHOMEBREW_ALLOW_FETCHCONTENT=ON"
-    system "./bin/compile"
-    system "./bin/install"
+    libexec_bin = libexec/"bin"
+    libexec_bin.install bin/"jank"
+    (bin/"jank").write <<~SH
+      #!/usr/bin/env bash
+      set -euo pipefail
+      exec "#{libexec_bin/"jank"}" "$@"
+    SH
+    (bin/"jank").chmod 0755
+    ln_s lib, libexec/"lib", force: true
   end
 
   test do
